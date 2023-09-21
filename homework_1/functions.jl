96ccf717a89ca36da791abebf501e01c65b7e824
ilyaboo <ibouligin@gmail.com>
2023-09-21 17:49:19 -0400

    adjusted the types of values that arrays store to signed integers

--- homework_1/functions.jl
+++ homework_1/functions.jl
@@ -10,19 +10,19 @@ function generate_random(r::UInt64)::UInt64
     # Optupt: r_new – new generated number
 
     # calculating the new number
-    r_new::UInt64 = a * r + c
+    r_new::Int64 = a * r + c
     return r_new
 
 end
 
-function extract_bits(r::UInt64)::Array{UInt64, 1}
+function extract_bits(r::UInt64)::Array{Int64, 1}
     # function that takes a generated number r
     # and extracts its 64 bits into the Array
     # Input: r – UInt64 number to extract bits from
     # Optupt: b – array of length 64 that stores
     # bits of r as separate elements
 
-    b = Array{UInt64, 1}(undef, 64) # initializing the array
+    b = Array{Int64, 1}(undef, 64) # initializing the array
 
     # iterating over bits
     for i in 1:64
@@ -32,13 +32,15 @@ function extract_bits(r::UInt64)::Array{UInt64, 1}
     return b
 end
 
-function update_walks(walks::Array{Array{UInt64, 1}, 1}, b::Array{UInt64, 1})
+function update_walks(walks::Array{Array{Int64, 1}, 1}, b::Array{Int64, 1})
     # function that updates the walks provided by the walks array
     # by adding the next values according to the steps arrat b
     # Input: walks – array with current paths
     #        b – array with steps needed to take
     for i in 1:length(walks)
+        b[i] = b[i] * 2 - 1
         append!(walks[i], walks[i][end] + b[i])
     end
 end
+
 
