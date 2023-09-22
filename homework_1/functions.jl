2c914287a3df4cbe05900eee04b2ecf131126e91
ilyaboo <ibouligin@gmail.com>
2023-09-22 12:49:04 -0400

    added the update_walks_with_seed and with b functions in order to create a function that can update walks just with the seed

--- homework_1/functions.jl
+++ homework_1/functions.jl
@@ -1,18 +1,16 @@
 # parameters of the random number generator
-a::UInt64 = 2862933555777941757
-c::UInt64 = 1013904243
+const a::UInt64 = 2862933555777941757
+const c::UInt64 = 1013904243
 
 
 function generate_random(r::UInt64)::UInt64
     # function that implements a linear congruential
     # generator
     # Input: r – last generated number
-    # Optupt: r_new – new generated number
+    # Optupt: new generated number
 
-    # calculating the new number
-    r_new::UInt64 = a * r + c
-    return r_new
-
+    # calculating the new number and returning it
+    return a * r + c
 end
 
 function extract_bits(r::UInt64)::Array{Int64, 1}
@@ -32,7 +30,7 @@ function extract_bits(r::UInt64)::Array{Int64, 1}
     return b
 end
 
-function update_walks(walks::Array{Array{Int64, 1}, 1}, b::Array{Int64, 1})
+function update_walks_with_b(walks::Array{Array{Int64, 1}, 1}, b::Array{Int64, 1})
     # function that updates the walks provided by the walks array
     # by adding the next values according to the steps arrat b
     # Input: walks – array with current paths
@@ -43,4 +41,13 @@ function update_walks(walks::Array{Array{Int64, 1}, 1}, b::Array{Int64, 1})
     end
 end
 
+function update_walks_with_seed(walks::Array{Array{Int64, 1}, 1}, seed::UInt64)
+    # function that utilizes extract_bits and update_walks_with_b to
+    # update walks array according to the randomly generated number
 
+    # extractring random bits
+    b = extract_bits(seed)
+
+    # updating walks
+    update_walks_with_b(walks, b)
+end
