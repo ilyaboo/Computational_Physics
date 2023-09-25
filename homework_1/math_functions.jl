17d758838f726890bd19f0b879630bb906d1adbd
ilyaboo <ibouligin@gmail.com>
2023-09-25 15:07:20 -0400

    created a file for mathematical functions, implemented probability_bit_value

---
+++ homework_1/math_functions.jl
@@ -0,0 +1,12 @@
+function probability_bit_value(num_steps::UInt64, val::Int64)::Float64
+    # function which calculates the expected probability
+    # of eventual value that correspinded to a bit being equal to val
+    # according to the formula for binomial distribution
+    # Input: num_steps – number of steps in a walk
+    #        val – eventual value after the walk is done
+    # Output: expected probability of obtaining such a value
+
+    # using the formula for binomial distribution
+    return (Float64(1) / 2^num_steps) * factorial(num_steps) / \
+        (factorial((num_steps + val) / 2) * factorial((num_steps - val) / 2))
+end
