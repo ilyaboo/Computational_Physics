812da9691cb5f9c06e518d0d9fe4bf9805d3131b
ilyaboo <ibouligin@gmail.com>
2023-09-25 03:55:48 -0400

    updated the input formats for functions; now Vector is used instead of 1D Array

--- homework_1/functions.jl
+++ homework_1/functions.jl
@@ -13,7 +13,7 @@ function generate_random(r::UInt64)::UInt64
     return a * r + c
 end
 
-function extract_bits(r::UInt64)::Array{Int64, 1}
+function extract_bits(r::UInt64)::Vector{Int64}
     # function that takes a generated number r
     # and extracts its 64 bits into the Array
     # Input: r – UInt64 number to extract bits from
@@ -30,7 +30,7 @@ function extract_bits(r::UInt64)::Array{Int64, 1}
     return b
 end
 
-function update_walks_with_b(walks::Array{Array{Int64, 1}, 1}, b::Array{Int64, 1})
+function update_walks_with_b(walks::Vector{Vector{Vector}}, b::Vector{Int64})
     # function that updates the walks provided by the walks array
     # by adding the next values according to the steps arrat b
     # Input: walks – array with current paths
@@ -41,7 +41,7 @@ function update_walks_with_b(walks::Array{Array{Int64, 1}, 1}, b::Array{Int64, 1
     end
 end
 
-function update_walks_with_seed(walks::Array{Array{Int64, 1}, 1}, seed::UInt64)
+function update_walks_with_seed(walks::Vector{Vector{Int64}}, seed::UInt64)
     # function that utilizes extract_bits and update_walks_with_b to
     # update walks array according to the randomly generated number
 
@@ -52,7 +52,7 @@ function update_walks_with_seed(walks::Array{Array{Int64, 1}, 1}, seed::UInt64)
     update_walks_with_b(walks, b)
 end
 
-function perform_walk(steps::UInt64, seed::UInt64)::Array{Array{Int64}}
+function perform_walk(steps::UInt64, seed::UInt64)::Vector{Vector{Int64}}
     # function that takes the number of steps as an argument
     # and the seed and performs the walk for a given number of steps
     # Input: steps – number of steps in the walk
--- homework_1/graph_functions.jl
+++ homework_1/graph_functions.jl
@@ -1,4 +1,4 @@
-function graph_walks(walks::Array{Array{Int64}})
+function graph_walks(walks::Vector{Vector{Int64}})
     for i in 1:64
         display(plot(0:100, walks[i]))
     end
