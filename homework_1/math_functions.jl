10a1527f760513a292b3f1d85ab12ead63f7a220
ilyaboo <ibouligin@gmail.com>
2023-09-25 15:47:31 -0400

    implemented expected_deviation

--- homework_1/math_functions.jl
+++ homework_1/math_functions.jl
@@ -11,3 +11,19 @@ function probability_bit_value(num_steps::UInt64, val::Int64)::Float64
             (factorial(big(div(num_steps + val, 2))) * 
             factorial(big(div(num_steps - val , 2))))
 end
+
+function expected_deviation(num_walks::UInt64, num_steps::UInt64, val::Int64, actual_count::UInt64)::Float64
+    # function that computes the expected deviation from the
+    # actual distribution
+    # Input: num_walks – number of walks performed
+    #        num_steps – number of steps in each walk'
+    #        val – the result value
+    #        actual_count – number of such resuls obtained
+    # Outupt: deviation from expected
+ 
+    # calculating the expected number of “hits”
+    C::Float64 = num_walks * probability_bit_value(num_steps, val)
+
+    # returning the difference after normalizing
+    return (C - actual_count) / num_walks
+end
