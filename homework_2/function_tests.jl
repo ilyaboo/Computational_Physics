a37e73156cf1a267ffbf4aedc51b025f1ff6184e
ilyaboo <ibouligin@gmail.com>
2023-10-03 18:40:01 -0400

    implemented first_order_integration, added tests for this function

--- homework_2/function_tests.jl
+++ homework_2/function_tests.jl
@@ -7,3 +7,15 @@ using Test
     @test f(0, 0, 1) == Inf
     @test f(-1, 0, 0) == 1
 end
+
+@testset "Testing discretization_step function" begin
+    @test discretization_step(5.0, UInt64(10)) == 0.5
+    @test discretization_step(0.0, UInt64(10)) == 0
+    @test discretization_step(3213.0, UInt64(31432423)) ≈ 0.00010221929
+    @test discretization_step(31432423.0, UInt64(3213)) ≈ 9782.88920012
+end
+
+@testset "Testing first_order_integration function" begin
+    @test round(first_order_integration(1000000.0, UInt64(1000000000), 1.0, 1.0), digits = 3) ≈ 13.816
+    @test round(first_order_integration(1000000.0, UInt64(1000000000), 1.0, 2.0), digits = 3) ≈ 1.000
+end
--- homework_2/math_functions.jl
+++ homework_2/math_functions.jl
@@ -1,13 +1,29 @@
-function f(x::Int64, epsilon::UInt64, alpha::UInt64)::Float64
+function f(x::Float64, epsilon::Float64, alpha::Float64)::Float64
     # function that is used in the assignment
     return 1 / (epsilon + x)^alpha
 end
 
-function discretization_step(x_N::UInt64, N::UInt64)::Float64
+function discretization_step(x_N::Float64, N::UInt64)::Float64
     # helper function that returns the discretization step
     # calculating it from upper bound and number of steps
     # (assuming lower bound is zero)
     return x_N / N
 end
 
-#function first_order_integration(x_N::UInt64, epsilon::UInt64, alpha::UInt64)
+function first_order_integration(x_N::Float64, N::UInt64, epsilon::Float64, alpha::Float64)::Float64
+    # function that given the parameters needed for integration returns the
+    # value of the first order integral according to the formula
+
+    # calculating the discretization step
+    h::Float64 = discretization_step(x_N, N)
+
+
+    # calculating the sum of the components
+    result::Float64 = 3/2 * f(h, epsilon, alpha) + 3/2 * f(h * (N - 1), epsilon, alpha)
+    for i in 2:(N - 2)
+        result += f(i * h, epsilon, alpha)
+    end
+
+    # returning the result multiplied by the step
+    return h * result
+end
