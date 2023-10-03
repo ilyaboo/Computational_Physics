1c8788faf4126b1fce90500a099c4547d0d0a442
ilyaboo <ibouligin@gmail.com>
2023-10-03 17:39:27 -0400

    implemented the discretization_step function, added tests for it

--- homework_2/math_functions.jl
+++ homework_2/math_functions.jl
@@ -1,4 +1,13 @@
-function f(x, epsilon, alpha)
+function f(x::Int64, epsilon::UInt64, alpha::UInt64)::Float64
     # function that is used in the assignment
     return 1 / (epsilon + x)^alpha
 end
+
+function discretization_step(x_N::UInt64, N::UInt64)::Float64
+    # helper function that returns the discretization step
+    # calculating it from upper bound and number of steps
+    # (assuming lower bound is zero)
+    return x_N / N
+end
+
+#function first_order_integration(x_N::UInt64, epsilon::UInt64, alpha::UInt64)
