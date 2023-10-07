ab796ce4832791e9bb456af8d91aefed8b50b68f
ilyaboo <ibouligin@gmail.com>
2023-10-07 16:47:34 -0400

    added files for constants and IO functions

---
+++ homework_3/constants.jl
@@ -0,0 +1,7 @@
+# constant values in SI units
+
+const G = 6.6743 * 10^-11   # Gravitational constant
+const Me = 5.9736 * 10^24   # Earth's mass
+const Mm = 7.3477 * 10^22   # Moon's mass
+const rm = 384400 * 10^3   # distance from moon to Earth's center
+const Ts = 86164   # length of siderial day
