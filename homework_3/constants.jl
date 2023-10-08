4c3d88540251f1ac375afebe7b6f987094b7c860
ilyaboo <ibouligin@gmail.com>
2023-10-08 00:45:03 -0400

    added new constants to fix the bug with large value of G

--- homework_3/constants.jl
+++ homework_3/constants.jl
@@ -3,6 +3,8 @@
 const G = 6.6743 * 10^-11   # Gravitational constant
 const Me = 5.9736 * 10^24   # Earth's mass
 const Mm = 7.3477 * 10^22   # Moon's mass
+const GMe = 3.9870 * 10^14   # product G * Me to avoid calculations with large value of G
+const GMm = 1.23 * 10^-2 * GMe   # product G * Mm to avoid calculations with large value of G
 const rs = 42168 * 10^3   # radius of the orbit of the satellite
 const rm = 384400 * 10^3   # distance from moon to Earth's center
 const Ts = 86164   # length of siderial day 
