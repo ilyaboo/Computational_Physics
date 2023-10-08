a557f8a32356fb2bd44a6c251d4b5e7bc0eb5026
ilyaboo <ibouligin@gmail.com>
2023-10-07 22:19:29 -0400

    added tests for get_moon_pos

--- homework_3/function_tests.jl
+++ homework_3/function_tests.jl
@@ -8,3 +8,10 @@ using Test
     @test get_accelerations((0.0, 42164000.0, 0.0), (0.0, 2 * 42164000.0, 0.0))[1] == 0
     @test get_accelerations((0.0, 42164000.0, 0.0), (0.0, 2 * 42164000.0, 0.0))[2] ≈ -G * Me / 42164000.0^2 + G * Mm / 42164000.0^2
 end
+
+@testset "Testing get_moon_pos function" begin
+    @test get_moon_pos(0.0, 0.0) == (rm, 0.0, 0.0)
+    @test get_moon_pos(Tm, 0.0)[1] == rm
+    @test abs(get_moon_pos(Tm, 0.0)[2]) < 10^-7
+    @test abs(get_moon_pos(Tm, 0.0)[3]) < 10^-7
+end
