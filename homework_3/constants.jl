c5a24fa09ba26582006f9c3f58b22ae09161e56f
ilyaboo <ibouligin@gmail.com>
2023-10-07 23:00:29 -0400

    added the orbit of the satellite

--- homework_3/constants.jl
+++ homework_3/constants.jl
@@ -3,6 +3,7 @@
 const G = 6.6743 * 10^-11   # Gravitational constant
 const Me = 5.9736 * 10^24   # Earth's mass
 const Mm = 7.3477 * 10^22   # Moon's mass
+const rs = 42168 * 10^3   # radius of the orbit of the satellite
 const rm = 384400 * 10^3   # distance from moon to Earth's center
 const Ts = 86164   # length of siderial day 
 const Tm = 27.25 * Ts   # period for the Moon
