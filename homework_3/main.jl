a39062bd2e02568a78baee6f6c602b67102aceb3
ilyaboo <ibouligin@gmail.com>
2023-10-09 16:47:38 -0400

    updated main to read the input from the user, run the simulation, and record the results

--- homework_3/main.jl
+++ homework_3/main.jl
@@ -1,4 +1,6 @@
 include("IO_functions.jl")
+include("math_functions.jl")
+include("constants.jl")
 
 alpha, Nt, tmax, Nw = read_user_input()
-println(alpha, Nt, tmax, Nw)
+run_simulation(alpha, Nt, tmax, Nw)
