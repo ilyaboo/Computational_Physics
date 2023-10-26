50f8c2632a33f616d3e485ee48b8e37d4d3dd13d
ilyaboo <ibouligin@gmail.com>
2023-10-26 01:45:08 -0400

    initial commit for constants

---
+++ homework_4/constants.jl
@@ -0,0 +1,5 @@
+const beta::Float64 = -0.053667   # constant for the radial Schrodinger equation
+const r0::Float64 = 0.1   # hard-core radius in fm
+const E::Float64 = -2.226   # binding energy of the deuteron in MeV
+const Mp::Float64 = 1.6726 * 10^-27   # mass of a proton in kg
+const Mn::Float64 = 1.6749 * 10^-27   # mass of a neutron in kg
