45eb2d1d1cdd3d4854bab83cd1044309a6669eca
ilyaboo <ibouligin@gmail.com>
2023-10-26 16:32:58 -0400

    implemenetd the function which iterates over the values of a and records the results into the file

--- homework_4/IO_functions.jl
+++ homework_4/IO_functions.jl
@@ -1,3 +1,5 @@
+include("math_functions.jl")
+
 function read_user_input()::Tuple{Float64, Float64}
     # function that reads the radial step and maximum radius for
     # integration from the console
@@ -9,4 +11,20 @@ function read_user_input()::Tuple{Float64, Float64}
     r_max = parse(Float64, readline())
 
     return delta_r, r_max
+end
+
+function write_results(r_max::Float64, delta_r::Float64)
+    # function which computes and records
+    # values of V0 and r for a range of values
+    # of a into the vr.dat file
+
+    file = open("vr.dat", "w")
+
+    for a in 0.5:0.01:3.0
+        V0 = find_bound_state(a, 0.001, r_max, delta_r)
+        r = compute_radius(V0, delta_r)
+        println(file, a, ",", V0, ",", r)
+    end
+
+    close(file)
 end
