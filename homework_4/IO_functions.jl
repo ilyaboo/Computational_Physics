763b8298ebd8f5e3c0ed79d4841a0bbaf565053b
ilyaboo <ibouligin@gmail.com>
2023-10-26 17:15:56 -0400

    removed the print statements

--- homework_4/IO_functions.jl
+++ homework_4/IO_functions.jl
@@ -22,10 +22,8 @@ function write_results(r_max::Float64, delta_r::Float64)
 
     for a in 0.5:0.1:3.0
         V0 = find_bound_state(a, 0.1, r_max, delta_r)
-        println("found state")
         r = compute_radius(V0, delta_r, r_max, a)
         println(file, a, ",", V0, ",", r)
-        println("checked for ", a)
     end
 
     close(file)
