ade9e6af3fec90a6257dbf66f9c5203847f79d0d
ilyaboo <ibouligin@gmail.com>
2023-10-27 14:42:47 -0400

    implemented the basis functions

---
+++ homework_5/math_functions.jl
@@ -0,0 +1,17 @@
+include("constants.jl")
+
+"""
+returns the value of the basis function `f_k_x`
+    for the given x-coordinate `x` and wave number `k_x`
+"""
+function f_k_x(x::Float64, k_x::Float64)::Float64
+    return sqrt(2 / Lx) * sin(k_x * π * x / Lx)
+end
+
+"""
+returns the value of the basis function `g_k_y`
+    for the given y-coordinate `y` and wave number `k_y`
+"""
+function g_k_y(y::Float64, k_y::Float64)::Float64
+    return sqrt(2 / Ly) * sin(k_y * π * y / Ly)
+end
