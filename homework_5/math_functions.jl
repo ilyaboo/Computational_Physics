cd1815083c686a6c876df111ea8954e2defa684b
ilyaboo <ibouligin@gmail.com>
2023-10-27 15:04:26 -0400

    implemented the get_state_index function

--- homework_5/math_functions.jl
+++ homework_5/math_functions.jl
@@ -16,5 +16,10 @@ function g_k_y(y::Float64, k_y::UInt64)::Float64
     return sqrt(2 / Ly) * sin(k_y * π * y / Ly)
 end
 
-
-
+"""
+function that returns the index of the state given
+    `k_x`, `k_y` and `N_x`
+"""
+function get_state_index(k_x::UInt64, k_y::UInt64, N_x::UInt64)::UInt64
+    return k_x + (k_y - 1) * N_x
+end
