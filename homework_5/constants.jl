955d857f58330af1386a930637603c107c1918b2
ilyaboo <ibouligin@gmail.com>
2023-10-27 14:02:46 -0400

    added relevant constants

---
+++ homework_5/constants.jl
@@ -0,0 +1,7 @@
+const Lx::Float64 = 5.0 * 10^-9   # width of semiconductor in m
+const Ly::Float64 = 10.0 * 10^-9   # length of semiconductor in m
+const a::Float64 = 1.0 * 10^-9   # length of the low potential areas in m
+const b::Float64 = 0.5 * 10^-9   # width of the low potential areas in m
+const x0::Float64 = 2.0 * 10^9   # distance from low potential are to the longer side of the demiconductor in m
+const y0::Float64 = 2.0 * 10^9   # distance from low potential are to the shorter side of the demiconductor in m
+const V0::Float64 = 0.1   # potential value
