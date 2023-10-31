b3e2296b40d2878346db8be22aa85b3b3bf83984
ilyaboo <ibouligin@gmail.com>
2023-10-31 17:34:35 -0400

    adjusted the constants by making nm the unit of all distances

--- homework_5/constants.jl
+++ homework_5/constants.jl
@@ -1,7 +1,7 @@
-const Lx::Float64 = 5.0 * 10^-9   # width of semiconductor in m
-const Ly::Float64 = 10.0 * 10^-9   # length of semiconductor in m
-const a::Float64 = 1.0 * 10^-9   # length of the low potential areas in m
-const b::Float64 = 0.5 * 10^-9   # width of the low potential areas in m
-const x0::Float64 = 2.0 * 10^9   # distance from low potential are to the longer side of the demiconductor in m
-const y0::Float64 = 2.0 * 10^9   # distance from low potential are to the shorter side of the demiconductor in m
+const Lx::Float64 = 5.0   # width of semiconductor in nm
+const Ly::Float64 = 10.0   # length of semiconductor in nm
+const a::Float64 = 1.0   # length of the low potential areas in nm
+const b::Float64 = 0.5   # width of the low potential areas in nm
+const x0::Float64 = 2.0   # distance from low potential are to the longer side of the demiconductor in nm
+const y0::Float64 = 2.0   # distance from low potential are to the shorter side of the demiconductor in nm
 const V0::Float64 = 0.1   # potential value
