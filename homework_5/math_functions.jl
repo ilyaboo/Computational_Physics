3464189ca8c34de5546a30333f851006e7533bb2
ilyaboo <ibouligin@gmail.com>
2023-11-03 15:15:23 -0400

    implemented check_if_inside_rectangles and get_potential_energy functions

--- homework_5/math_functions.jl
+++ homework_5/math_functions.jl
@@ -31,3 +31,39 @@ function that returns the kinetic enery term
 function get_beta_times_kinetic_energy(k_x::UInt64, k_y::UInt64)::Float64
     return π^2 / 2 * ((Float64(k_x) / L_x)^2 + (Float64(k_y) / L_y)^2)
 end
+
+"""
+function which checks if the given pair of coordinates `(x, y)` corresponds
+    to one of the two rectangular regions that have a potential of V0
+"""
+function check_if_inside_rectangles(x::Float64, y::Float64)::Bool
+    
+    # horizontal check 
+    if x >= x0 && x <= Lx - x0
+        
+        # vertical check for bottom rectangle 
+        if y >= y0 && y <= y0 + b
+            return true
+        
+        # vertical check for top rectangle
+        elseif y >= Ly - y0 - b && y <= Ly - y0
+            return true
+        end
+    end
+    return false
+end
+
+"""
+function that returns the potnetial energy term of the 
+    variational Hamiltonian using `k_x`, `p_x`, `k_y`, `p_y`, `x`, `y`
+"""
+function get_potential_energy(k_x::UInt64, p_x::UInt64, k_y::UInt64, p_y::UInt64, x::Float64, y::Float64)::Float64
+    if check_if_inside_rectangles(x, y)
+        return V0 / π^2 * ((sin(π * x * (Float64(p_x) - Float64(k_x)) / Lx) / (Float64(p_x) - Float64(k_x))) -
+                (sin(π * x * (Float64(p_x) + Float64(k_x)) / Lx) / (Float64(p_x) + Float64(k_x)))) *
+                ((sin(π * y * (Float64(p_y) - Float64(k_y)) / Ly) / (Float64(p_y) - Float64(k_y))) -
+                (sin(π * y * (Float64(p_y) + Float64(k_y)) / Ly) / (Float64(p_y) + Float64(k_y))))
+    else
+        return 0.0
+    end
+end
