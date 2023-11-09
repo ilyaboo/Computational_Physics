25acf3229e07972464224ffb5d083c8d345ec537
ilyaboo <ibouligin@gmail.com>
2023-11-08 23:11:40 -0500

    implemented write_wave_function_to_file

--- homework_5/IO_functions.jl
+++ homework_5/IO_functions.jl
@@ -9,3 +9,23 @@ function read_input()::Tuple{UInt64, UInt64}
     N_y = parse(UInt64, readline())
     return N_x, N_y
 end
+
+"""
+function whcih writes the wave function `wave_function` to
+    the file named `filename`
+"""
+function write_wave_function_to_file(wave_function::Array{Float64,2}, filename::String)
+
+    f = open(filename, "w")
+
+    # iterating over wave function values
+    for i in 1:size(wave_function, 1)
+        for j in 1:size(wave_function, 2)
+            print(f, "$(wave_function[i, j]) ")
+        end
+
+        print(f, "\n")
+    end
+
+    close(f)
+end
