3073e750670a6bc3272cf2c9103c022ee6f645ed
ilyaboo <ibouligin@gmail.com>
2023-11-10 23:36:09 -0500

    changed the value of the potential to a negative one

--- homework_5/constants.jl
+++ homework_5/constants.jl
@@ -4,5 +4,5 @@ const a::Float64 = 1.0   # length of the low potential areas in nm
 const b::Float64 = 0.5   # width of the low potential areas in nm
 const x0::Float64 = 2.0   # distance from low potential are to the longer side of the demiconductor in nm
 const y0::Float64 = 2.0   # distance from low potential are to the shorter side of the demiconductor in nm
-const V0::Float64 = 0.1   # potential value
+const V0::Float64 = -0.1   # potential value
 const β::Float64 = 13.11   # constant from Schrodinger eqaution in 1/(eV*nm^2)
