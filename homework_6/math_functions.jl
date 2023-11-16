8cea09357df00fb9a1e8340be7d0fd8aa63c26d8
ilyaboo <ibouligin@gmail.com>
2023-11-16 17:58:33 -0500

    implemented get_magnetization

---
+++ homework_6/math_functions.jl
@@ -0,0 +1,12 @@
+"""
+function which calculates magnetization `m` of the state
+"""
+function get_magnetization(state::Vector{Int64, 2})::Float64
+    total::Float64 = 0.0
+    for i in 1:length(state)
+        for j in 1:length(state[1])
+            total += state[i][j]
+        end
+    end
+    return total / (length(state) * length(state[1]))
+end
