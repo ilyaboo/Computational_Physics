fa2b799e34afd4bb1144bce94b9e1a036e2aba01
ilyaboo <ibouligin@gmail.com>
2023-11-17 00:01:32 -0500

    implemented bin_average_and_error

--- homework_6/math_functions.jl
+++ homework_6/math_functions.jl
@@ -1,3 +1,5 @@
+using Statistics
+
 """
 function which calculates magnetization `m` of the state
 """
@@ -9,4 +11,22 @@ function get_magnetization(state::Vector{Vector{Int64}})::Float64
         end
     end
     return total / (length(state) * length(state[1]))
+end
+
+"""
+function that calculate the average magnetizations and errors
+    for a given values of a bin
+"""
+function bin_average_and_error(magnetization_runs_bin::Vector{Vector{Float64}})::Tuple{Vector{Float64}, Vector{Float64}}
+    averages::Vector{Float64} = []
+    errors::Vector{Float64} = []
+    for step in 1:length(magnetization_runs_bin)
+        average_value = mean(magnetization_runs_bin[step])
+        push!(averages, average_value)
+
+        # calculating error as maximum absolute deviation from average
+        push!(errors, max((max(magnetization_runs_bin[step]) - average_value), (average_value - min(magnetization_runs_bin[step]))))
+    end
+    
+    return averages, errors
 end
