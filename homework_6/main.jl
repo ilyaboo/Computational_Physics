2dde9241d457a449bbd2f79e9a19eff8fd0a4c32
ilyaboo <ibouligin@gmail.com>
2023-11-17 00:38:00 -0500

    reimplemented the logic of main

--- homework_6/main.jl
+++ homework_6/main.jl
@@ -1,43 +1,48 @@
 include("IO_functions.jl")
 include("math_functions.jl")
 
+# setting the name for input and output files
+filename_read = "read.in"
+filenmae_write_magnetizations = "res.dat"
+
 # reading input from the file
-L, T, bins, reps, steps = read_input()
+L, T, bins, reps, steps = read_input(filename_read)
 
-# 2D vector for storing magnetizations for different returns
-magnetization_runs::Vector{Vector{Float64}} = []
 
-# running the simulation reps number of times
-for _ in 1:reps
+# сhecking if the number of steps is not zero (first part)
+if steps == zero
 
-    # storing magnetization values
-    magnetization_vals::Vector{Float64} = []
+    # 2D vector for storing magnetizations for different runs,
+    # where i-th row represents magnetizations on the n-th step
+    magnetization_runs::Vector{Vector{Float64}} = [[] for _ in 1:steps]
 
-    # initial state
-    state::Vector{Vector{Int64}} = [[1 for _ in 1:L] for _ in 1:L]
+    # running the simulation reps number of times
+    for _ in 1:reps
 
-    # adding initial magnetization
-    push!(magnetization_vals, get_magnetization(state))
+        # initial state
+        state::Vector{Vector{Int64}} = [[1 for _ in 1:L] for _ in 1:L]
 
-    # conducting steps for Monte Carlo simulation
-    for _ in 1:steps
+        # conducting steps for Monte Carlo simulation
+        for step in 1:steps
 
-        # creating new state
-        state_new::Vector{Vector{Int64}} = [[0 for _ in 1:L] for _ in 1:L]
+            # creating new state
+            state_new::Vector{Vector{Int64}} = [[0 for _ in 1:L] for _ in 1:L]
 
-        # applying Monte Carlo algorithm
-        ...
-        
-        # recording new magnetization
-        push!(magnetization_vals, get_magnetization(state_new))
+            # applying Monte Carlo algorithm
+            ...
+            
+            # recording new magnetization
+            push!(magnetization_runs[step], get_magnetization(state_new))
 
-        # updating current state
-        state = state_new
+            # updating current state
+            state = state_new
+        end
     end
 
-    # adding current run to magnetization_runs
-    push!(magnetization_runs, magnetization_vals)
-end
+    # creatring a vector of average magnetizations on each step
+    average_magnetizations::Vector{Float64} = [mean(magnetization_runs[i]) for i in 1:length(magnetization_runs)]
 
-# saving magnetization runs to the file
-...
+    # recording average magnetizations
+
+# otherwise, 0 steps (second part)
+else
