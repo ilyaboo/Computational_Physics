330733557806953c0a3b226ac2bab4d4244451d2
ilyaboo <ibouligin@gmail.com>
2023-11-24 02:28:46 -0500

    implemented function for lin-log graphing

--- homework_6/graphing.py
+++ homework_6/graphing.py
@@ -22,12 +22,38 @@ def get_steps_avergaes_errors(filename: str):
     f.close()
     return steps, magnetizations, errors
 
-def graph_magnetizations(steps: list[int], magnetizations: list[float], errors: list[float]):
-    # function which produces the graph for magnetization
+def graph_magnetizations_lin(steps: list[int], magnetizations: list[float], errors: list[float], include_errors: bool):
+    # function which produces the graph for magnetization using lin-lin scale
 
-    plt.plot(steps, magnetizations)
-    plt.errorbar(steps, magnetizations, errors)
+    if include_errors:
+        plt.errorbar(steps, magnetizations, errors)
+    else:
+        plt.plot(steps, magnetizations)
+
+    # labels
+    plt.xlabel("Steps")
+    plt.ylabel("Average Magnetization")
+    plt.title("Average Magnetization vs. Steps (Lin-Lin Scale)")
+    plt.grid(True)
+    plt.show()
+
+def graph_magnetizations_log(steps: list[int], magnetizations: list[float], errors: list[float], include_errors: bool):
+    # function which produces the graph for magnetization using lin-log scale
+
+    if include_errors:
+        plt.errorbar(steps, magnetizations, yerr = errors)
+    else:
+        plt.plot(steps, magnetizations)
+
+    # setting y-axis to logarithmic scale
+    plt.yscale('log')
+
+    # labels
+    plt.xlabel("Steps")
+    plt.ylabel("Logarithm of Average Magnetization")
+    plt.title("Log of Average Magnetization vs. Steps (Lin-Log Scale)")
+    plt.grid(True)
     plt.show()
 
 steps, magnetizations, errors = get_steps_avergaes_errors("graphing.dat")
-graph_magnetizations(steps, magnetizations, errors)
+graph_magnetizations_lin(steps, magnetizations, errors, False)
