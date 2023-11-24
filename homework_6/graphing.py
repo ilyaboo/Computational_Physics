b15d64c9fb4bfa8e0d474878d21f5c4ba1ebec54
ilyaboo <ibouligin@gmail.com>
2023-11-24 11:48:35 -0500

    implemented graph_steps

--- homework_6/graphing.py
+++ homework_6/graphing.py
@@ -67,7 +67,53 @@ def graph_magnetizations_log(steps: list[int], magnetizations: list[float], erro
     plt.title("Log of Average Magnetization vs. Steps (Lin-Log Scale)")
     plt.legend()
     plt.grid(True)
+    plt.tight_layout()
     plt.show()
 
-steps, magnetizations, errors = get_steps_avergaes_errors("graphing.dat")
-graph_magnetizations_lin(steps, magnetizations, errors, False)
+def graph_steps(L_vals: list[int], step_vals: list[int], type: int):
+    # function which produces the graphs for the average number of steps 
+    # needed to reach zero magnetization for different values of L
+    # type specifies the type of the scales
+
+    plt.plot(L_vals, step_vals)
+
+    if type == 1:
+        plt.xlabel("L values")
+        plt.ylabel("Average Number of Steps")
+        plt.title("Average Number of Steps vs. L Values (Lin-Lin Scale)")
+
+    elif type == 2:
+        plt.yscale('log')
+
+        plt.xlabel("L values")
+        plt.ylabel("Logarithm of the Average Number of Steps")
+        plt.title("Average Number of Steps vs. L Values (Lin-Log Scale)")
+
+    elif type == 3:
+        plt.xscale('log')
+        plt.yscale('log')
+
+        plt.xlabel("Logarithm of L values")
+        plt.ylabel("Logarithm of the Average Number of Steps")
+        plt.title("Average Number of Steps vs. L Values (Log-Log Scale)")
+
+    elif type == 4:
+        plt.xscale('log')
+
+        plt.xlabel("Logarithm of L values")
+        plt.ylabel("Average Number of Steps")
+        plt.title("Average Number of Steps vs. L Values (Log-Lin Scale)")
+
+    else:
+        raise Exception("Incorrect type of the graph")
+        
+    plt.legend()
+    plt.grid(True)
+    plt.tight_layout()
+    plt.show()
+
+#steps, magnetizations, errors = get_steps_avergaes_errors("graphing.dat")
+#graph_magnetizations_log(steps, magnetizations, errors, True)
+
+#vals_2 = [182, 838, 3262, 12334, 39257]
+#vals_21 = [
