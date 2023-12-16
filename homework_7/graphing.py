85d8c8aac8c3ee61c486a6e51cc7af93617c4a9a
ilyaboo <ibouligin@gmail.com>
2023-12-15 20:09:23 -0500

    adjusted the read input function and graphing function for part 2 to normalize the time axis

--- homework_7/graphing.py
+++ homework_7/graphing.py
@@ -11,7 +11,7 @@ def parse_results(filename: str, total_time: int) -> tuple[list[float], list[flo
         Es.append(vals[2])
     return times, Ps, Es
 
-def parse_results_2(filename: str, total_time: int) -> list[tuple[list[float], list[float], list[float], list[float], list[float]]]:
+def parse_results_2(filename: str) -> list[tuple[list[float], list[float], list[float], list[float], list[float]]]:
         """ function which parses the ferro.jl output for part 2 of the assignment """
         f = open(filename, "r")
         results = []
@@ -35,7 +35,7 @@ def parse_results_2(filename: str, total_time: int) -> list[tuple[list[float], l
             
             last_time = vals[0]
 
-            times.append(vals[0] * total_time)
+            times.append(vals[0])
             Ps_avgs.append(vals[1])
             Ps_errors.append(vals[2])
             Es_avgs.append(vals[3])
@@ -58,9 +58,9 @@ def graph_Ps(folder_path: str, num_steps: list[int], total_time: int, title: str
         # calculating dt
         dt = 1 / step_num
 
-        plt.plot(times, Ps, "--", label = "dt = " + str(dt) + " s")
+        plt.plot(times, Ps, "--", label = "dt = " + str(dt))
 
-    plt.xlabel("time, s")
+    plt.xlabel("time")
     plt.ylabel("Success Rate")
     plt.title(title)
     plt.grid()
@@ -79,26 +79,26 @@ def graph_Es(folder_path: str, num_steps: list[int], total_time: int, title: str
         # calculating dt
         dt = 1 / step_num
 
-        plt.plot(times, Es, "--", label = "dt = " + str(dt) + " s")
+        plt.plot(times, Es, "--", label = "dt = " + str(dt))
 
-    plt.xlabel("time, s")
+    plt.xlabel("time")
     plt.ylabel("Excess Energy")
     plt.title(title)
     plt.grid()
     plt.legend()
     plt.show()
 
-def plot_Ps_Es_errors(data: tuple[list[float], list[float], list[float], list[float], list[float]], N: int, reps: int):
+def plot_Ps_Es_errors(data: tuple[list[float], list[float], list[float], list[float], list[float]], N: int, T: int, reps: int):
     """ function which plots Ps vs time and Es vs time grraphs with error bars """
 
     bin_times, bin_Ps, bin_Ps_errors, bin_Es, bin_Es_errors = data
 
     # ending of the graph titles
-    ending = "for system of size N = " + str(N) + ", velocity of v = " + str(1 / bin_times[-1]) + "\nand number of repetitions of reps = " + str(reps)
+    ending = "for system of size N = " + str(N) + ", velocity of v = " + str(1 / T) + "\nand number of repetitions of reps = " + str(reps)
 
     # plotting Ps
     plt.errorbar(bin_times, bin_Ps, yerr = bin_Ps_errors, markersize = 3, linestyle = "--", fmt = 'o', ecolor = 'red', capsize = 1)
-    plt.xlabel("time, s")
+    plt.xlabel("time, t / T")
     plt.ylabel("Success Rate")
     plt.title("Success Rate VS Time\n" + ending)
     plt.grid()
@@ -107,7 +107,7 @@ def plot_Ps_Es_errors(data: tuple[list[float], list[float], list[float], list[fl
 
     # plotting Es
     plt.errorbar(bin_times, bin_Es, yerr = bin_Es_errors, markersize = 3, linestyle = "--", fmt = 'o', ecolor = 'red', capsize = 1)
-    plt.xlabel("time, s")
+    plt.xlabel("time, t / T")
     plt.ylabel("Excess Energy")
     plt.title("Escess Energy VS Time\n" + ending)
     plt.grid()
@@ -117,11 +117,11 @@ def plot_Ps_Es_errors(data: tuple[list[float], list[float], list[float], list[fl
 
 
 # part 1
-#graph_Ps("./data_10", [10, 20, 25, 50, 100, 1000, 10000], total_time = 10, title = "Success Rate VS Time for T = 10")
-#graph_Es("./data_10", [10, 20, 25, 50, 100, 1000, 10000], total_time = 10, title = "Excess Energy VS Time for T = 10")
+graph_Ps("./data_10", [10, 20, 25, 50, 100, 1000, 10000], total_time = 10, title = "Success Rate VS Time for T = 10")
+graph_Es("./data_10", [10, 20, 25, 50, 100, 1000, 10000], total_time = 10, title = "Excess Energy VS Time for T = 10")
 
-#graph_Ps("./data_100", [10, 20, 25, 50, 100, 1000, 10000], total_time = 100, title = "Success Rate VS Time for T = 100")
-#graph_Es("./data_10", [10, 20, 25, 50, 100, 1000, 10000], total_time = 100, title = "Excess Energy VS Time for T = 100")
+graph_Ps("./data_100", [10, 20, 25, 50, 100, 1000, 10000], total_time = 100, title = "Success Rate VS Time for T = 100")
+graph_Es("./data_10", [10, 20, 25, 50, 100, 1000, 10000], total_time = 100, title = "Excess Energy VS Time for T = 100")
     
 
 
